5d140887605421bd42d2d5870fe01c002d05fe53
unknown <jeyoungcs@gmail.com>
2017-11-30 18:06:35 +0700

    create api

---
+++ contacts/index.js
@@ -0,0 +1,59 @@
+    const express = require('express')
+    const router = express.Router()
+
+    let friend = [
+           { 
+               fname:"",
+               lname:"",
+               email:"",
+               phone:"",
+               url:"",
+               note:""
+           }
+        ]
+
+    //get tew
+
+
+
+
+
+
+
+
+
+
+    //post chon
+
+
+
+
+
+
+
+
+
+
+    //put zuche
+
+
+
+
+
+
+
+
+
+
+    //delete je
+
+
+
+
+
+
+
+
+
+    
+    module.exports = router
--- server/index.js
+++ server/index.js
@@ -1,30 +1,11 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const contactsRouter = require('../contacts')
 const app = express()
 
 app.use(bodyparser.urlencoded({extended:false}))
 app.use(bodyparser.json())
 
-var friends = [
-    { name: 'dom', phone: '08754632663' },
-    { name: 'robin', phone: '08754631234' }     
-]
+app.use('/', contactsRouter)
 
-app.get('/friend/:id', (req,res) =>{
-    let id = req.params.id
-    res.json(friends[id])
-})
-
-app.get('/friend', (req,res) => {
-    res.json(friends)
-})
-
-app.post('/friend', (req,res) => {
-    let newFriend = res.body.name
-    friends.push(newFriend)
-})
-
-app.listen(3000, () => console.log('Example server'))
-
-
-
+module.exports = app
