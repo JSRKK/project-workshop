89e577a49224259a44780ded87f41aa2cbb92388
unknown <jeyoungcs@gmail.com>
2017-12-01 19:37:02 +0700

    create api get finish

--- contacts/index.js
+++ contacts/index.js
@@ -1,29 +1,26 @@
-    const express = require('express')
-    const router = express.Router()
+const express = require('express')
+const router = express.Router()
+const friends = require('./db')
 
-    let friend = [
-           { 
-               fname:"",
-               lname:"",
-               email:"",
-               phone:"",
-               url:"",
-               note:""
-           }
-        ]
+//get tew
+router.get('/contacts', (req, res) => {
+    let name = req.query.name
+    let resFriend = []
+    if (name) {
+        for (let i = 0; i < friends.length; i++) {
+            if (friends[i].fname === name)
+                resFriend.push(friends[i])
+        }
+        res.json(resFriend)
+    }
+    else {
+        res.json(friends)
+    }
+})
 
-    //get tew
-
-
-
-
-
-
-
-
-
-
-    //post chon
+router.get('/contacts/:id', (req, res) => {
+    res.json(friends.find(friends => friends.id === req.params.id))
+})
 
 
 
@@ -34,7 +31,8 @@
 
 
 
-    //put zuche
+
+//post chon
 
 
 
@@ -45,7 +43,7 @@
 
 
 
-    //delete je
+//put zuche
 
 
 
@@ -55,5 +53,16 @@
 
 
 
-    
-    module.exports = router
+
+//delete je
+
+
+
+
+
+
+
+
+
+
+module.exports = router
